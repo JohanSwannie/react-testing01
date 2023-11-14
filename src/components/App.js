5ef98fa2532bc56d7e9bb57bb5ddc83897ce82e0
Johan Swan <jswannieman@gmail.com>
2023-11-15 12:06:00 +1300

    Update App JS Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import Grid from './listings/Grid';
-import courses from '../data/courses.json';
+import React, { Component } from "react";
+import "./App.css";
+import Grid from "./listings/Grid";
+import courses from "../data/courses.json";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {courses};
+    this.state = { courses };
   }
 
   render() {
     return (
       <div>
+        {" "}
         <div className="navbar-fixed">
           <nav className="blue lighten-2">
             <div className="nav-wrapper">
-              <a href="/" className="brand-logo center">Courses</a>
+              <a href="/" className="brand-logo center" id="app-atag">
+                Courses
+              </a>
             </div>
           </nav>
         </div>
         <div>
-            <Grid items={this.state.courses}/>
+          <Grid items={this.state.courses} />
         </div>
       </div>
     );
