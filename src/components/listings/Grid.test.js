178ef6767c090552a837adc83960940da952a0a2
Johan Swan <jswannieman@gmail.com>
2023-11-15 12:07:03 +1300

    Commit Grid.test JS Component
    undefined

---
+++ src/components/listings/Grid.test.js
@@ -0,0 +1,17 @@
+import data from "../../data/courses.json";
+
+const numItems = data.length;
+
+test("number of items = 12", () => {
+  expect(numItems).toBe(12);
+});
+
+test("number of items to be greater than or equal to 12", () => {
+  expect(numItems).toBeGreaterThanOrEqual(12);
+});
+
+const dataTest = data[0].title;
+
+test("There is the letters 'JS' in this title", () => {
+  expect(dataTest).toMatch(/JS/);
+});
