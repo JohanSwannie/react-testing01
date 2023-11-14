46d4dcf2ab9e040b4e7cdfbb29ded31f01594d1f
Johan Swan <jswannieman@gmail.com>
2023-11-15 12:06:30 +1300

    Update App.test JS Component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  root.render(<App />, div);
+test("renders the text 'Courses'", () => {
+  render(<App />);
+  const textDisplay = document.getElementById("app-atag").innerHTML;
+  expect(textDisplay).toMatch(/Courses/i);
 });
