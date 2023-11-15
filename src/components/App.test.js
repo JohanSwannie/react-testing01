60f1d1be34ce14304a1208206d4144be2537b25d
Johan Swan <jswannieman@gmail.com>
2023-11-15 16:23:46 +1300

    Update App.test JS Component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import renderer from "react-test-renderer";
 import App from "./App";
 
 test("renders the text 'Courses'", () => {
   render(<App />);
   const textDisplay = document.getElementById("app-atag").innerHTML;
   expect(textDisplay).toMatch(/Courses/i);
+});
+
+test("App snapshot test", () => {
+  const component = renderer.create(<App />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
