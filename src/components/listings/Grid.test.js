dfecf14b2a2043d92a5fd6700f23c9137ee0d16a
Johan Swan <jswannieman@gmail.com>
2023-11-15 16:47:10 +1300

    Update Grid.test JS Component

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,45 +1,45 @@
 import data from "../../data/courses.json";
 
-//* Numbers Test
+//* Numbers Tests
 
 const numItems = data.length;
 
-test("number of items = 12", () => {
-  expect(numItems).toBe(12);
+describe("Number Tests", () => {
+  test("number of items = 12", () => {
+    expect(numItems).toBe(12);
+  });
+  test("number of items to be greater than or equal to 12", () => {
+    expect(numItems).toBeGreaterThanOrEqual(12);
+  });
 });
 
-test("number of items to be greater than or equal to 12", () => {
-  expect(numItems).toBeGreaterThanOrEqual(12);
-});
-
-//* Strings Test
+//* Strings Tests
 
 const dataTest = data[0].title;
 
-test("There is the letters 'JS' in this title", () => {
-  expect(dataTest).toMatch(/JS/);
+describe("String Tests", () => {
+  test("There is the letters 'JS' in this title", () => {
+    expect(dataTest).toMatch(/JS/);
+  });
+  test("There is the word 'React' in this title", () => {
+    expect(dataTest).toContain("React");
+  });
 });
 
-test("There is the word 'React' in this title", () => {
-  expect(dataTest).toContain("React");
-});
-
-//* Array Test
+//* Arrays & Objects Tests
 
 const data2 = ["React Native", "React"];
 
-test("The list of courses contains 'React Native' and 'React'", () => {
-  expect(["React Native", "React", "Another One"]).toEqual(
-    expect.arrayContaining(data2)
-  );
-});
-
-//* Objects Test
-
-test("The 1st course to have a property 'title'", () => {
-  expect(data[0]).toHaveProperty("title");
-});
-
-test("The 1st course to have a property 'title' and a value of '31,266'", () => {
-  expect(data[0]).toHaveProperty("views", "31,266");
+describe("Arrays & Objects Tests", () => {
+  test("The list of courses contains 'React Native' and 'React'", () => {
+    expect(["React Native", "React", "Another One"]).toEqual(
+      expect.arrayContaining(data2)
+    );
+  });
+  test("The 1st course to have a property 'title'", () => {
+    expect(data[0]).toHaveProperty("title");
+  });
+  test("The 1st course to have a property 'title' and a value of '31,266'", () => {
+    expect(data[0]).toHaveProperty("views", "31,266");
+  });
 });
