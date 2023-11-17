ac1af01ae9b9c365b17262a1ecd43554743cf141
Johan Swan <jswannieman@gmail.com>
2023-11-17 20:05:15 +1300

    Update index JS Component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-serviceWorker.unregister();
