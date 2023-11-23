0d7a8248f19468bc9ee9a883dacc362bf4c75ca3
Johan Swan <jswannieman@gmail.com>
2023-11-23 16:10:56 +1300

    Update App.test JS Component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -9,6 +9,11 @@ test("renders the text 'Course List'", () => {
   expect(textDisplay.length).toBeGreaterThanOrEqual(11);
 });
 
+// When the following test is run, a folder called __snapshots__ will be created with
+// a file calles App.test.js.snap, where the snapshot will be visible. When the component
+// App.js is changed, the snapshot will detect the changes and throw an error. You can use
+// u for update in the terminal console to sync your changes
+
 test("App snapshot test", () => {
   const component = renderer.create(<App />);
   const tree = component.toJSON();
